8590db6a2c380c2a196a29d24814a80a593b4b28
Samir Ghosh <hello@samir.tech>
2023-11-13 21:38:59 -0800

    added the cartwheeler back in

--- src/Experience/World/Pingu.js
+++ src/Experience/World/Pingu.js
@@ -18,18 +18,18 @@ export default class Pingu {
     this.scene.add(this.model);
     this.action.play();
 
-    //same thing as above but with samirModel
-    // this.resource2 = this.resources.items.samirModel;
-    // this.model2 = this.resource2.scene;
-    // this.model2.scale.set(0.85, 0.85, 0.85);
-    // console.log(this.model2);
-    // // this.model2.position.z -= 4;
+    // same thing as above but with samirModel
+    this.resource2 = this.resources.items.samirModel;
+    this.model2 = this.resource2.scene;
+    this.model2.scale.set(-0.85, 0.85, -0.85);
+    console.log(this.model2);
+    // this.model2.position.z -= 4;
     // this.model2.position.x += 2;
     // this.model2.rotation.y -= Math.PI / 2;
-    // this.mixer2 = new THREE.AnimationMixer(this.model2);
-    // this.action2 = this.mixer2.clipAction(this.resource2.animations[0]);
-    // this.scene.add(this.model2);
-    // this.action2.play();
+    this.mixer2 = new THREE.AnimationMixer(this.model2);
+    this.action2 = this.mixer2.clipAction(this.resource2.animations[0]);
+    this.scene.add(this.model2);
+    this.action2.play();
 
     // this.pinguGeometry, this.pinguMaterial = ... import glb file
   }
@@ -37,7 +37,7 @@ export default class Pingu {
     if (this.mixer) {
       this.mixer.update(this.experience.time.delta * 0.0018);
       //mixer2
-      // this.mixer2.update(this.experience.time.delta * 0.002);
+      this.mixer2.update(this.experience.time.delta * 0.0018);
     }
   }
 }
