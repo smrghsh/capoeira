44ed28df55ec0270f6b23130141bf74382ef6d2d
Samir Ghosh <hello@samir.tech>
2024-12-07 22:08:09 -0800

    clean: reformat

--- src/Experience/Utils/Resources.js
+++ src/Experience/Utils/Resources.js
@@ -1,89 +1,58 @@
-import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import EventEmitter from './EventEmitter.js'
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import EventEmitter from "./EventEmitter.js";
 
-export default class Resources extends EventEmitter
-{
-    constructor(sources)
-    {
-        super()
-        this.sources = sources
-        // Setup
-        this.items = {}
-        this.toLoad = this.sources.length
-        this.loaded = 0
-        this.setLoaders()
-        this.startLoading()
+export default class Resources extends EventEmitter {
+  constructor(sources) {
+    super();
+    this.sources = sources;
+    // Setup
+    this.items = {};
+    this.toLoad = this.sources.length;
+    this.loaded = 0;
+    this.setLoaders();
+    this.startLoading();
+  }
 
+  setLoaders() {
+    this.loaders = {};
+    this.loaders.gltfLoader = new GLTFLoader();
+    this.loaders.textureLoader = new THREE.TextureLoader();
+    this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader();
+    this.loaders.fontLoader = new THREE.FontLoader();
+  }
+  startLoading() {
+    // Load each source
+    for (const source of this.sources) {
+      if (source.type === "gltfModel") {
+        this.loaders.gltfLoader.load(source.path, (file) => {
+          this.sourceLoaded(source, file);
+        });
+      } else if (source.type === "texture") {
+        this.loaders.textureLoader.load(source.path, (file) => {
+          this.sourceLoaded(source, file);
+        });
+      } else if (source.type === "cubeTexture") {
+        this.loaders.cubeTextureLoader.load(source.path, (file) => {
+          this.sourceLoaded(source, file);
+        });
+      } else if (source.type === "font") {
+        this.loaders.fontLoader.load(source.path, (file) => {
+          this.sourceLoaded(source, file);
+        });
+      }
     }
+  }
 
-    setLoaders()
-    {
-        this.loaders = {}
-        this.loaders.gltfLoader = new GLTFLoader()
-        this.loaders.textureLoader = new THREE.TextureLoader()
-        this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader()
-        this.loaders.fontLoader = new THREE.FontLoader()
+  sourceLoaded(source, file) {
+    this.items[source.name] = file;
+
+    this.loaded++;
+
+    if (this.loaded === this.toLoad) {
+      let loading = document.getElementById("loading");
+      loading.style.display = "none";
+      this.trigger("ready");
     }
-    startLoading()
-    {
-        // Load each source
-        for(const source of this.sources)
-        {
-            if(source.type === 'gltfModel')
-            {
-                this.loaders.gltfLoader.load(
-                    source.path,
-                    (file) =>
-                    {
-                        this.sourceLoaded(source, file)
-                    }
-                )
-            }
-            else if(source.type === 'texture')
-            {
-                this.loaders.textureLoader.load(
-                    source.path,
-                    (file) =>
-                    {
-                        this.sourceLoaded(source, file)
-                    }
-                )
-            }
-            else if(source.type === 'cubeTexture')
-            {
-                this.loaders.cubeTextureLoader.load(
-                    source.path,
-                    (file) =>
-                    {
-                        this.sourceLoaded(source, file)
-                    }
-                )
-            }
-            else if(source.type === 'font')
-            {
-                this.loaders.fontLoader.load(
-                    source.path,
-                    (file) =>
-                    {
-                        this.sourceLoaded(source, file)
-                    }
-                )
-            }
-        }
-    }
-
-    sourceLoaded(source, file)
-    {
-        this.items[source.name] = file
-
-        this.loaded++
-
-        if(this.loaded === this.toLoad)
-        {
-            let loading = document.getElementById('loading')
-            loading.style.display = 'none'
-            this.trigger('ready')
-        }
-    }
+  }
 }
